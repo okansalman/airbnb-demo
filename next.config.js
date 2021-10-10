40d744059ebf04677778ad72a1cd0986ac6846c3
Okan Salman <okan.salman@msn.com>
2021-10-10 21:03:44 +0200

    day two build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     images: {
         domains: ['links.papareact.com']
+    },
+    env: {
+        mapbox_style: 'mapbox://styles/okansalman/ckulg4fb6fx4219nsh9egvuuo',
+        mapbox_key: 'pk.eyJ1Ijoib2thbnNhbG1hbiIsImEiOiJja2Nkbjcxa24wMDU4MnpueGM3ZnUydXhkIn0.z80oLxOi6U-CA3qrS_8EJA'
     }
 };
